cea3891a0026a69c8cdf8bd683a565fa9d3affc0
Muhammad0830 <abduqayumovm1@gmail.com>
2025-07-26 18:27:09 +0500

    job card icons modal is done

--- frontend/components/JobCard.tsx
+++ frontend/components/JobCard.tsx
@@ -1,12 +1,11 @@
 import { Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
   faFileContract,
   faUtensils,
   faGlobe,
   faCoins,
-  faShoePrints,
   faHome,
   faTrain,
   faCalendarAlt,
@@ -24,13 +23,12 @@ import {
   faArrowUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
+import FontAwesomeIconWithInfo from "@/components/FontAwesomeIconWithInfo";
+import BigInfoModal from "./BigInfoModal";
 
 export default function JobCard({ Job }: any) {
   const { t } = useTranslation();
-
-  const handleInfoPress = () => {
-    alert("ここにはないよ");
-  };
+  const [bigInfoModalOpen, setBigInfoModalOpen] = useState(false);
 
   const typeIconMap: Record<string, any> = {
     utensils: faUtensils,
@@ -71,16 +69,18 @@ export default function JobCard({ Job }: any) {
       <View className={`bg-white w-full rounded-2xl flex-1 p-2`}>
         <View className="flex-row justify-between items-center gap-3 px-2 py-4">
           <View className="flex-row gap-3 items-center">
-            <View
-              style={{ padding: 20 }}
-              className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-            >
-              <FontAwesomeIcon icon={faBuilding} size={24} color="blue" />
+            <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+              <FontAwesomeIconWithInfo
+                icon={faBuilding}
+                size={24}
+                color="blue"
+                hasInfo
+              />
             </View>
             <Text className="text-2xl font-bold">{Job.name}</Text>
           </View>
           <TouchableOpacity
-            onPress={() => handleInfoPress()}
+            onPress={() => setBigInfoModalOpen(true)}
             className="justify-center items-center p-2 rounded-full bg-[#c29c70]"
           >
             <FontAwesomeIcon icon={faInfo} size={18} color="white" />
@@ -89,17 +89,19 @@ export default function JobCard({ Job }: any) {
         <View className="gap-5 mt-2 flex-1 justify-evenly border border-[#c29c70] rounded-2xl p-3">
           <View className="flex-row items-center justify-between gap-3">
             <View className="flex-row items-center gap-3">
-              <View
-                style={{ padding: 20 }}
-                className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-              >
-                <FontAwesomeIcon icon={faFileContract} size={24} color="blue" />
+              <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+                <FontAwesomeIconWithInfo
+                  icon={faFileContract}
+                  size={24}
+                  color="blue"
+                  hasInfo
+                />
               </View>
               <View>
                 <Text className="text-lg font-bold">{t(`${Job.type}`)}</Text>
               </View>
             </View>
-            <View className="p-3 border border-[#c29c70] rounded-full justify-center items-center">
+            <View className="border border-[#c29c70] p-2.5 rounded-full justify-center items-center">
               <FontAwesomeIcon
                 icon={typeIconMap[Job.icon]}
                 size={24}
@@ -112,11 +114,13 @@ export default function JobCard({ Job }: any) {
 
           <View className="flex-row items-center justify-between gap-3 py-2">
             <View className="w-[53%] flex-row items-center gap-3">
-              <View
-                style={{ padding: 20 }}
-                className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-              >
-                <FontAwesomeIcon icon={faCoins} size={24} color="blue" />
+              <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+                <FontAwesomeIconWithInfo
+                  icon={faCoins}
+                  size={24}
+                  color="blue"
+                  hasInfo
+                />
               </View>
               <View>
                 <Text className="text-sm font-bold">{Job.payment}</Text>
@@ -124,11 +128,13 @@ export default function JobCard({ Job }: any) {
             </View>
             <View className="w-[1px] border border-l-[#c29c70] border-r-0 h-[150%] "></View>
             <View className="w-[46%] flex-row items-center gap-1">
-              <View
-                style={{ padding: 20 }}
-                className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-              >
-                <FontAwesomeIcon icon={faGlobe} size={24} color="blue" />
+              <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+                <FontAwesomeIconWithInfo
+                  icon={faGlobe}
+                  size={24}
+                  color="blue"
+                  hasInfo
+                />
               </View>
               <View className="flex-1 items-center mr-2">
                 <Text className="text-lg font-bold">{minLevel}</Text>
@@ -140,17 +146,13 @@ export default function JobCard({ Job }: any) {
 
           <View className="flex-row items-center justify-between gap-3 pr-3">
             <View className="w-[55%] flex-row items-center gap-4">
-              <View
-                style={{ padding: 20 }}
-                className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-              >
-                <FontAwesomeIcon icon={faHome} size={24} color="blue" />
-                <View
-                  style={{ transform: [{ rotate: "-90deg" }] }}
-                  className="absolute -bottom-2 -right-2 border border-[#c29c70] bg-white p-1 rounded-full"
-                >
-                  <FontAwesomeIcon icon={faShoePrints} size={15} color="blue" />
-                </View>
+              <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+                <FontAwesomeIconWithInfo
+                  icon={faHome}
+                  size={24}
+                  color="blue"
+                  hasInfo
+                />
               </View>
               <View>
                 <Text className="text-lg font-bold">
@@ -161,11 +163,13 @@ export default function JobCard({ Job }: any) {
             </View>
             <View className="w-[1px] border border-l-[#c29c70] border-r-0 h-[100%] "></View>
             <View className="w-[44%] flex-row items-center gap-1">
-              <View
-                style={{ padding: 20 }}
-                className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-              >
-                <FontAwesomeIcon icon={faTrain} size={24} color="blue" />
+              <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+                <FontAwesomeIconWithInfo
+                  icon={faTrain}
+                  size={24}
+                  color="blue"
+                  hasInfo
+                />
               </View>
               <View className="gap-1 flex-1 items-center">
                 <View className="border border-[#c29c70] rounded-full p-1 justify-center items-center w-14 aspect-square">
@@ -183,11 +187,13 @@ export default function JobCard({ Job }: any) {
           <View className="border border-x-0 border-b-0 border-t-[#c29c70]"></View>
 
           <View className="flex-row items-center justify-between gap-3 pr-1">
-            <View
-              style={{ padding: 20 }}
-              className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-            >
-              <FontAwesomeIcon icon={faCalendarAlt} size={24} color="blue" />
+            <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+              <FontAwesomeIconWithInfo
+                icon={faCalendarAlt}
+                size={24}
+                color="blue"
+                hasInfo
+              />
             </View>
             <View>
               <View className="flex-row items-center gap-1">
@@ -224,11 +230,13 @@ export default function JobCard({ Job }: any) {
           <View className="border border-x-0 border-b-0 border-t-[#c29c70]"></View>
 
           <View className="flex-row justify-between gap-3 pb-2 pr-1 items-center">
-            <View
-              style={{ padding: 20 }}
-              className="border border-[#c29c70] rounded-full w-[35px] justify-center items-center aspect-square"
-            >
-              <FontAwesomeIcon icon={faStar} size={24} color="blue" />
+            <View className="border border-[#c29c70] rounded-full justify-center items-center aspect-square">
+              <FontAwesomeIconWithInfo
+                icon={faStar}
+                size={24}
+                color="blue"
+                hasInfo
+              />
             </View>
             <View className="border border-[#c29c70] rounded-md p-2 flex-1 flex-row items-center gap-2 flex-wrap">
               {starIconConfig
@@ -250,6 +258,13 @@ export default function JobCard({ Job }: any) {
         </View>
       </View>
       <View className="transparent w-full" style={{ height: 70 }}></View>
+
+      <View>
+        <BigInfoModal
+          isBigInfoModalOpen={bigInfoModalOpen}
+          setBigInfoModalOpen={setBigInfoModalOpen}
+        />
+      </View>
     </View>
   );
 }
